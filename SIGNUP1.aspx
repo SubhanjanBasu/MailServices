4926a5a930cc559fea4d6d0b6911dbbbdcc6438b
SubhanjanBasu <subhanjan.basu12@gmail.com>
2016-06-07 12:02:37 +0000

    Add files via upload

---
+++ SIGNUP1.aspx
@@ -0,0 +1,197 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SIGNUP1.aspx.cs" Inherits="SIGNUP1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head runat="server">
+
+  <meta charset="UTF-8">
+
+  <title>Budgetize</title>
+
+    <style>
+@import url(http://fonts.googleapis.com/css?family=Exo:100,200,400);
+@import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
+
+body{
+	margin: 0;
+	padding: 0;
+	background: #fff;
+
+	color: #fff;
+	font-family: Arial;
+	font-size: 12px;
+}
+
+.body{
+	position: absolute;
+	top: -20px;
+	left: -20px;
+	right: -40px;
+	bottom: -40px;
+	width: auto;
+	height: auto;
+	background-image: url(img/bg1.jpg);
+	background-size: cover;
+	-webkit-filter: blur(5px);
+	z-index: 0;
+}
+
+.grad{
+	position: absolute;
+	top: -20px;
+	left: -20px;
+	right: -40px;
+	bottom: -40px;
+	width: auto;
+	height: auto;
+	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
+	z-index: 1;
+	opacity: 0.7;
+}
+
+.header{
+	position: absolute;
+	top: 171px;
+	left: 291px;
+	z-index: 2;
+}
+
+.header div{
+	float: left;
+	color: #fff;
+	font-family: 'Exo', sans-serif;
+	font-size: 35px;
+	font-weight: 200;
+}
+
+.header div span{
+	color: #5379fa !important;
+}
+
+.login{
+	position: absolute;
+	top: 188px;
+	left: 668px;
+	height: 150px;
+	width: 350px;
+	padding: 10px;
+	z-index: 2;
+}
+
+.login input[type=text]{
+	width: 250px;
+	height: 30px;
+	background: transparent;
+	border: 1px solid rgba(255,255,255,0.6);
+	border-radius: 2px;
+	color: #fff;
+	font-family: 'Exo', sans-serif;
+	font-size: 16px;
+	font-weight: 400;
+	padding: 4px;
+}
+
+.login input[type=password]{
+	width: 250px;
+	height: 30px;
+	background: transparent;
+	border: 1px solid rgba(255,255,255,0.6);
+	border-radius: 2px;
+	color: #fff;
+	font-family: 'Exo', sans-serif;
+	font-size: 16px;
+	font-weight: 400;
+	padding: 4px;
+	margin-top: 10px;
+}
+
+.login input[type=button]{
+	width: 260px;
+	height: 35px;
+	background: #fff;
+	border: 1px solid #fff;
+	cursor: pointer;
+	border-radius: 2px;
+	color: #a18d6c;
+	font-family: 'Exo', sans-serif;
+	font-size: 16px;
+	font-weight: 400;
+	padding: 6px;
+	margin-top: 10px;
+}
+
+.login input[type=button]:hover{
+	opacity: 0.8;
+}
+
+.login input[type=button]:active{
+	opacity: 0.6;
+}
+
+.login input[type=text]:focus{
+	outline: none;
+	border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=password]:focus{
+	outline: none;
+	border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=button]:focus{
+	outline: none;
+}
+
+::-webkit-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+
+::-moz-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+</style>
+
+    <script src="js/prefixfree.min.js"></script>
+
+</head>
+
+<body runat="server">
+
+    <form id="form1" runat="server">
+
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="TextBox2" ErrorMessage="AGE MUST BE IN NUMBER" style="z-index: 1; left: 955px; top: 256px; position: absolute" ValidationExpression="[0123456789]+$"></asp:RegularExpressionValidator>
+
+  <div class="body"></div>
+		<div class="grad"></div>
+		<div class="header">
+			<div>CUSTOMER<span>SIGNUP</span></div>
+		</div>
+		
+		<div class="login">
+				<asp:TextBox ID="TextBox1" runat="server" placeholder="FULLNAME"></asp:TextBox>
+            
+                
+                <asp:TextBox ID="TextBox2" runat="server" placeholder="AGE"></asp:TextBox>
+                <asp:TextBox ID="TextBox3" runat="server" placeholder="USERNAME"></asp:TextBox>
+        <asp:TextBox ID="Email" runat="server" placeholder="Email"></asp:TextBox>
+        <asp:TextBox ID="TextBox4" runat="server" placeholder="PASSWORD" TextMode="Password"></asp:TextBox>
+        <asp:TextBox ID="TextBox5" runat="server" placeholder="CONFIRM PASSWORD" TextMode="Password"></asp:TextBox>
+        <asp:CompareValidator ID="CompareValidator1" runat="server" ErrorMessage="Password does not match" ControlToCompare="TextBox4" ControlToValidate="TextBox5" style="z-index: 1; left: 282px; top: 244px; position: absolute"></asp:CompareValidator>
+        
+                <br>
+				<input type="button" value="SignUp" runat="server" id="Button1" OnServerClick="Button1_OnClick">
+         
+        
+		</div>
+        
+
+  <script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>
+
+    </form>
+
+</body>
+
+</html>
---
+++ SIGNUP1.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net.Mail;
+using System.Net;
+
+public partial class SIGNUP1 : System.Web.UI.Page
+{
+    dbConnection cn = new dbConnection();
+    dbConnection cn1 = new dbConnection();
+    SqlDataReader dr;
+    string email,username,pass,fullname;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void Button1_OnClick(object sender, EventArgs e)
+    {
+        if (TextBox1.Text.ToString() == "" || TextBox2.Text.ToString() == "" || TextBox3.Text.ToString() == "" || TextBox4.Text.ToString() == "" || TextBox5.Text.ToString() == "")
+        {
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "scriptkey", "<script>alert('FILL UP ALL THE FIELDS');</script>");
+        }
+        else
+        {
+            //Table Name SIGNUP
+
+            cn.sqlQuery("SELECT * FROM SIGNUP");
+            dr = cn.getdata;
+
+            while (dr.Read())
+            {
+                if ((dr.GetValue(2).ToString() == TextBox1.Text) || (dr.GetValue(4).ToString() == Email.Text))
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "scriptkey", "<script>alert('USERNAME or Email ALREADY EXISTS');</script>");
+                    TextBox3.Text = "";
+                    Email.Text = "";
+                }
+
+            }
+           
+                cn.closeDataReader();
+                cn.closeCommand();
+                cn.closeConnection();
+
+                if (Page.IsValid)
+                {
+                    try
+                    {
+                        string random = genCode();
+                        email = Email.Text.ToString();
+                        fullname = TextBox1.Text.ToString();
+                        username = TextBox3.Text.ToString();
+                        pass = TextBox5.Text.ToString();
+                        cn.insupdel("INSERT INTO SIGNUP VALUES('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text + "','" + Email.Text + "','" + random + "')");
+                        cn.closeDataReader();
+                        cn.closeConnection();
+                        cn.closeCommand();
+                        sendMsg(email, fullname, username, pass, random);
+                        Session["newEmail"] = username;
+                        Response.Redirect("Activation.aspx");
+                        
+                    }
+                    catch (Exception msg1)
+                    {
+                        Response.Write(msg1);
+                    }
+
+
+
+                }
+            }
+                
+            
+            cn.closeDataReader();
+            cn.closeCommand();
+            cn.closeConnection();
+
+
+            
+        
+    }
+
+    //Generate Random Number
+    public String genCode()
+    {
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var stringChars = new char[8];
+        var random = new Random();
+
+        for (int i = 0; i < stringChars.Length; i++)
+        {
+            stringChars[i] = chars[random.Next(chars.Length)];
+        }
+
+        var finalString = new String(stringChars);
+        return finalString;
+    }
+    
+    
+    public static void sendMsg(string email,string fullname,string username,string pass,string random)
+    {
+
+        SmtpClient smtp = new SmtpClient();
+        smtp.Host = "smtp.gmail.com";
+        smtp.Port = 587;
+        smtp.Credentials = new System.Net.NetworkCredential("project2016budgetizer@gmail.com", "budgetizer2016");
+        smtp.EnableSsl = true;
+
+        MailMessage msg = new MailMessage();
+        msg.Subject = "Hello "+fullname;
+        msg.Body = "Hello "+fullname+"Thanks for Registering in Budgetize...Your Account Details are given below:";
+        msg.Body += "<tr>";
+        msg.Body += "<td>User Name :" + username + "</td>";
+        msg.Body += "</tr>";
+        msg.Body += "<tr>";
+        msg.Body +="<td>Password :" + pass + "</td>";
+        msg.Body += "</tr>";
+        msg.Body += "<tr>";
+        msg.Body += "<td>Activation Number :" + random + "</td>";
+        msg.Body += "</tr>";
+
+        msg.Body += "<tr>";
+        msg.Body += "<td>Thanking</td><td>Team Budgetize</td>";
+        msg.Body += "</tr>";
+
+        string toAddress = email; // Add Recepient address
+        msg.To.Add(toAddress);
+
+        string fromAddress = "\"Budgetize \" <project2016budgetizer@gmail.com>";
+        msg.From = new MailAddress(fromAddress);
+        msg.IsBodyHtml = true;
+
+        try
+        {
+            smtp.Send(msg);
+
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
